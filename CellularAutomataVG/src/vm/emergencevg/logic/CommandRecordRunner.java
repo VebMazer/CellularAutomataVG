2e1e0b60b2a53f4ea6ef01acad62a06e85569835
VebMazer <veikko.laine@gmail.com>
2023-07-15 00:38:42 +0300

    Translated CommandRecordRunner comments to English and added some line breaks to make the code easier to read. Added an updated build.

--- CellularAutomataVG/src/vm/emergencevg/logic/CommandRecordRunner.java
+++ CellularAutomataVG/src/vm/emergencevg/logic/CommandRecordRunner.java
@@ -5,40 +5,45 @@ import java.util.HashMap;
 import vm.emergencevg.ui.Updatable;
 
 /**
- * Ylläpitää aikaa iteraatioiden suoritusmäärän muodossa ja tallentaa käyttäjän
- * interaktioita logiikka-avaruuden kanssa suhteessa siihen.
+ * Includes the iteration count and saves user interactions that happen on
+ * each iteration.
  */
 public class CommandRecordRunner {
 
     Environment environment;
     public int iterations;
+    
     public HashMap<Integer, ArrayList<String>> commands;
     public HashMap<Integer, ArrayList<String>> commandsToBeAdded;
+    
     public ArrayList<String> presets;
     public ArrayList<String> presetsToBeAdded;
+    
     Updatable uiIterationDisplayer;
 
     /**
-     * Konstruktori joka vastaanottaa Environment tyypin olion, jonka kautta
-     * luokka saa muut arvonsa. Komentojoukot(commands, presets) vaativat
-     * apukomento joukon(commandsToBeAdded, presetsToBeAdded) ylläptioon siksi
-     * ettei komentojen lisääminen suorituksen aikana, häiritse ohjelman
-     * suoritusta.
+     * Constructor receives the environment object which includes the
+     * necessary values for this class. The command containers (commands and presets)
+     * are initialized. The -ToBeAdded containers are used when commands or presets
+     * are added so that the commands dont get issued twice. (once by the user and 
+     * once by the CommandRecordRunner.)
      *
-     * @param environment Ohjelman logiikka-avaruutta ja taustalooppia ylläpitävä
-     * muuttuja.
+     * @param environment Object that includes the main loop controlling the program,
+     * the particle board, and its properties.
      */
     public CommandRecordRunner(Environment environment) {
         this.environment = environment;
         iterations = 0;
+        
         commands = new HashMap<Integer, ArrayList<String>>();
         commandsToBeAdded = new HashMap<Integer, ArrayList<String>>();
+        
         presets = new ArrayList<String>();
         presetsToBeAdded = new ArrayList<String>();
     }
 
     /**
-     * Uudellen alustetaan komennot.
+     * Reinitialize commands.
      */
     public void reInitializeCommands() {
         commandsToBeAdded = new HashMap<Integer, ArrayList<String>>();
@@ -46,7 +51,7 @@ public class CommandRecordRunner {
     }
 
     /**
-     * Uudellen alustetaan preset komennot.
+     * Reinitialize preset lists.
      */
     public void reInitializePresets() {
         presetsToBeAdded = new ArrayList<String>();
@@ -54,11 +59,12 @@ public class CommandRecordRunner {
     }
 
     /**
-     * Suorittaa preset komennot.
+     * Applies the preset commands in the preset variable.
      */
     public void runPresets() {
         ArrayList<String> temp = presets;
         presets = new ArrayList<String>();
+        
         for (String command : temp) {
             runPresetCommand(command);
         }
@@ -72,7 +78,10 @@ public class CommandRecordRunner {
      */
     public void runPresetCommand(String command) {
         if (command.charAt(0) == 'l') {
-            environment.functions.processVariablesToParticle(command.substring(2, findDoubleComma(2, command)), parseDisplayAttributes(command));
+            environment.functions.processVariablesToParticle(
+                command.substring(2, findDoubleComma(2, command)),
+                parseDisplayAttributes(command)
+            );
         } else if (command.charAt(0) == 'f') {
             parseAndSetFieldSize(command);
         }
@@ -93,84 +102,95 @@ public class CommandRecordRunner {
     }
 
     /**
-     * Parseroi partikkelin määrittelykomennosta sen piirtoon liittyvät
-     * muuttujat.
+     * Parses the display attributes that define how a particle is drawn from
+     * a preset command that defines a particle.
      *
-     * @param command Komento tekstimuodossa.
-     * @return listan partikkelin piirtoon liittyvistä Integer muuttujista.
+     * @param command Includes the preset command in a string.
+     * @return A list of the display attributes which are represented by integers.
      */
     public ArrayList<Integer> parseDisplayAttributes(String command) {
         ArrayList<Integer> displayAttributes = new ArrayList<Integer>();
+        
         int index = findDoubleComma(0, command);
         index += 2;
         index = environment.uFunctions.parseNextNumber(index, command, displayAttributes);
+        
         environment.uFunctions.parseNextNumber(index, command, displayAttributes);
+        
         return displayAttributes;
     }
 
     /**
-     * Etsii ",," merkkijonon komennosta ja palauttaa sen alkamis indeksin.
+     * Finds the string: ",," from a command and returns its starting index.
      *
-     * @param index Indeksi, josta etsityn merkkijonon etsintä aloitetaan.
-     * @param command Komennon merkkijono, josta merkkijonoa ",," etsitään.
-     * @return Indeksi, josta etsitty merkkijono alkoi, tai komennon viimeinen
-     * indeksi
+     * @param index Indeksi, josta etsityn merkkijonon etsintä aloitetaan. Index from which the search begins.
+     * @param command Command string from which the ",," separator sign is searched from.
+     * @return Index from which ",," started from or the last index of the command.
      */
     public int findDoubleComma(int index, String command) {
         while (index < command.length() - 1) {
-            if (command.substring(index, index + 2).equals(",,")) {
-                break;
-            }
+            
+            if (command.substring(index, index + 2).equals(",,")) break;
             index++;
         }
         return index;
     }
 
     /**
-     * Suorittaa komennot, jotka on määrätty suoritettavalle iteraatiolle.
+     * Runs the commands that match the the iteration in iterations.
      */
     public void runCommands() {
         ArrayList<String> commandList = commands.get(iterations);
+        
         if (commandList != null) {
-            for (String command : commandList) {
-                runCommand(command);
-            }
+            
+            for (String command : commandList) runCommand(command);
         }
     }
 
     /**
-     * Suorittaa komennon.
+     * Runs a command.
      *
-     * @param command Suoritettava komento String muodossa.
+     * @param command Command in String form.
      */
     public void runCommand(String command) {
         if (environment.uFunctions.checkIfNumber(command.charAt(1))) {
             parsePlacementStringToPlaceIt(command);
+        
         } else if (command.equals("clear")) {
             environment.functions.clear();
+        
         } else if (command.substring(0, 5).equals("speed")) {
             environment.functions.setSpeed(command.substring(6, command.length() - 1));
+        
         } else if (command.substring(0, 2).equals("sc")) {
             environment.functions.setScale(Integer.parseInt(command.substring(6, command.length() - 1)));
+        
         } else if (command.charAt(0) == 'f') {
             parseAndSetFieldSize(command);
         }
     }
     
     /**
-     * Parseroidaan partikkelin sijoituskomennosta tarvittavat int arvot ja
-     * sijoitetaan partikkeli.
+     * Parses the integer values needed for the placement of a particle. Which include:
+     * the particle key and the x and y coordinates.
      *
-     * @param command Suoritettava komento String muodossa.
+     * @param command The command to be processed in string form.
      */
     public void parsePlacementStringToPlaceIt(String command) {
         int index = 1;
+        
         Integer particleKey = environment.uFunctions.parseNumber(command, index);
+        
         if (environment.particles.containsKey(particleKey)) {
             index += particleKey.toString().length() + 1;
+            
             Integer x = environment.uFunctions.parseNumber(command, index);
+            
             index += x.toString().length() + 1;
+            
             int y = environment.uFunctions.parseNumber(command, index);
+            
             if (x < environment.width && y < environment.height) {
                 environment.functions.placeParticle(particleKey, x, y);
             }
